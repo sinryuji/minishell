3951f06d68e4325a4c4f66bc6ab2073509164a7b
sinryuji <siler2474@gmail.com>
2022-10-03 20:02:16 +0900

    Feat: Makefile

--- source/main.c
+++ source/main.c
@@ -6,11 +6,31 @@
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/01 15:58:39 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/10/01 16:07:25 by jiwahn           ###   ########.fr       */
+/*   Updated: 2022/10/03 19:57:20 by hyeongki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char *argv[])
+#include "../include/token.h"
+#include "../include/as_tree.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+
+int	main(int argc, char **argv, char **envp)
 {
+	char	*line;
+
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (line)
+		{
+
+		}
+		else
+		{
+
+		}
+	}
 	return (0);
 }
