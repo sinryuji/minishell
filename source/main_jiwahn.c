feb41e4c7a9a957d7b0c78eac81a3c8182f5389e
안지우 <ilp_rincipe@naver.com>
2022-10-07 17:23:23 +0900

    Test: Add test code

--- source/main_jiwahn.c
+++ source/main_jiwahn.c
@@ -1,23 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parser.c                                           :+:      :+:    :+:   */
+/*   main_jiwahn.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/04 09:05:29 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/10/07 17:05:22 by jiwahn           ###   ########.fr       */
+/*   Created: 2022/10/07 17:06:33 by jiwahn            #+#    #+#             */
+/*   Updated: 2022/10/07 17:21:33 by jiwahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <errno.h>
-
-#include "../include/parser.h"
-
-void	parser(t_token *toks, t_tree **root)
-{
-
-}
+/*
+ * main function for testing parser and scanner
+ * */
 
 #include <stdio.h>
 #include <readline/readline.h>
@@ -32,18 +27,36 @@ void	print_toks(t_token *toks)
 	printf("\n");
 }
 
+void	traverse_tree(t_tree *root)
+{
+	if (root->right == NULL && root->left == NULL)
+	{
+		printf("(%d): %s\n", root->type, root->tok->text);
+		return ;
+	}
+	traverse_tree(root->right);
+	traverse_tree(root->left);
+}
+
+void	leak_test(void)
+{
+	system("leaks a.out");
+}
+
 int main(int argc, char *argv[])
 {
 	t_token		*toks;
 	t_tree		*root;
 	char		*input;
 
+	atexit(leak_test);
 	toks = NULL;
 	input = readline("input>> ");
 	scanner(&toks, input);
-	parser(toks, &root);
+	printf("\nscanner executed\n");
 	print_toks(toks);
-	system("leaks a.out");
+	parser(toks, &root);
+	printf("\n\nparser executed\n");
 	free(input);
 	return (0);
 }
