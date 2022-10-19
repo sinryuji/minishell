a0d1c60b06b6d215d8ab505f5e42347f72f238cb
jiwoo ahn <ilp_rincipe@naver.com>
2022-10-19 20:18:47 +0900

    Fix: expand_env err fix

--- source/expand_env.c
+++ source/expand_env.c
@@ -6,7 +6,7 @@
 /*   By: jiwahn <jiwahn@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 18:39:20 by jiwahn            #+#    #+#             */
-/*   Updated: 2022/10/19 19:47:56 by jiwahn           ###   ########.fr       */
+/*   Updated: 2022/10/19 20:18:10 by jiwahn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,14 @@
 static int	is_allowed(char c)
 {
 	return (ft_isalnum(c) || c == '-');
+}
+
+static char	*get_env_val(t_env *env)
+{
+	if (env == NULL)
+		return (NULL);
+	else
+		return (env->value);
 }
 
 static char	*strjoin_3way(char *pre, char *mid, char *post)
@@ -56,21 +64,25 @@ static int	replace_text(t_token *toks, int start, t_env_list *envl)
 	char	*mid;
 	char	*post;
 
-	//find key(env var)
 	i = start;
 	while(toks->text[i] && is_allowed(toks->text[i]))
 		i++;
 	key = ft_strndup(toks->text + start, i - start);
-	//get_sub_str
+	mid = get_env_val(get_env(envl, key));
 	pre = ft_substr(toks->text, 0, start - 1);
-	mid = get_env(envl, key)->value;
 	post = ft_substr(toks->text, i, ft_strlen(toks->text) - i);
-	i = ft_strlen(mid);
-	//replace
 	free(toks->text);
-	toks->text = strjoin_3way(pre, mid, post);
+	if (mid != NULL)
+	{
+		i = ft_strlen(mid);
+		toks->text = strjoin_3way(pre, mid, post);
+	}
+	else
+	{
+		i = 0;
+		toks->text = ft_strjoin(pre, post);
+	}
 	free(pre);
-	free(mid);
 	free(post);
 	free(key);
 	return (i);
